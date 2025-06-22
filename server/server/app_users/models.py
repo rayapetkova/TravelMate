e59bc6523dacea39e8d543bb32a0dcb109a3c84f
Raya Petkova <rayapetkova2007@abv.bg>
2025-06-22 16:55:24 +0300

    Create custom model AppUser

--- server/server/app_users/models.py
+++ server/server/app_users/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from server.app_users.managers import AppUserManager
+
+
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+        null=False,
+        blank=False,
+        error_messages={
+            'unique': "User with this email already exists."
+        }
+    )
+
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+
+    USERNAME_FIELD = 'email'
+    objects = AppUserManager()
